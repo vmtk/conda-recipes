3d11a483f24734fbaa1928a3237a2b47c615dbc6
Rick Izzo <rizzo242@gmail.com>
2017-12-24 03:22:08 -0500

    updates for windows

--- vmtk/bld.bat
+++ vmtk/bld.bat
@@ -1,39 +1,50 @@
-## Program:   VMTK
-## Module:    Anaconda Distribution
-## Language:  Python
-## Date:      May 29, 2017
+:: Program:   VMTK
+:: Module:    Anaconda Distribution
+:: Language:  Python
+:: Date:      May 29, 2017
 
-##   Copyright (c) Richard Izzo, Luca Antiga, David Steinman. All rights reserved.
-##   See LICENCE file for details.
+::   Copyright (c) Richard Izzo, Luca Antiga, David Steinman. All rights reserved.
+::   See LICENCE file for details.
 
-##      This software is distributed WITHOUT ANY WARRANTY; without even
-##      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-##      PURPOSE.  See the above copyright notices for more information.
+::      This software is distributed WITHOUT ANY WARRANTY; without even
+::      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+::      PURPOSE.  See the above copyright notices for more information.
 
-## Note: this script was contributed by
-##       Richard Izzo (Github @rlizzo)
-##       University at Buffalo; The Jacobs Institute
+:: Note: this script was contributed by
+::       Richard Izzo (Github @rlizzo)
+::       University at Buffalo; The Jacobs Institute
 
-## This file contains the packaging and distribution shell script data for packaging
-## VMTK via the Continuum Analytics Anaconda Python distribution.
-## See https://www.continuum.io/ for distribution info
+:: This file contains the packaging and distribution shell script data for packaging
+:: VMTK via the Continuum Analytics Anaconda Python distribution.
+:: See https://www.continuum.io/ for distribution info
 
 mkdir wk
 cd wk
 
-set BUILD_CONFIG="RELEASE"
+:: tell cmake where Python is
+set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
 
-cmake -LAH .. -G "Ninja" ^
-	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-	-DCMAKE_BUILD_TYPE=Release ^
-	-DVMTK_INSTALL_BIN_DIR=%LIBRARY_BIN% ^
-	-DVMTK_INSTALL_INC_DIR=%LIBRARY_INC% ^
-	-DVMTK_INSTALL_LIB_DIR=%LIBRARY_LIB% ^
+cmake .. -G "Ninja" ^
+    -DSUPERBUILD_INSTALL_PREFIX:STRING=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE:STRING="Release" ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DBUILD_SHARED_LIBS:BOOL=ON ^
+    -DVMTK_RENDERING_BACKEND:STRING="OpenGL2" ^
     -DUSE_SYSTEM_VTK:BOOL=ON ^
     -DUSE_SYSTEM_ITK:BOOL=ON ^
-	-DPYTHON_EXECUTABLE=%PYTHON% ^
-	-DVMTK_USE_SUPERBUILD:BOOL=ON ^
-	-DSUPERBUILD_INSTALL_PREFIX=%PREFIX%
+    -DPYTHON_EXECUTABLE:FILEPATH="%PYTHON%" ^
+    -DPYTHON_INCLUDE_DIR:PATH="%PREFIX%/include" ^
+    -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
+    -DINSTALL_BIN_DIR="%LIBRARY_BIN%" ^
+    -DINSTALL_INC_DIR="%LIBRARY_INC%" ^
+    -DINSTALL_LIB_DIR="%LIBRARY_LIB%" ^
+    -DVMTK_SCRIPTS_INSTALL_BIN_DIR="%LIBRARY_BIN%" ^
+    -DVMTK_SCRIPTS_INSTALL_LIB_DIR="%LIBRARY_LIB%/python%PY_VER:~0,1%.%PY_VER:~2,1%/site-packages/vmtk" ^
+    -DPYPES_INSTALL_BIN_DIR="%LIBRARY_BIN%" ^
+    -DPYPES_MODULE_INSTALL_LIB_DIR="%LIBRARY_LIB%/python%PY_VER:~0,1%.%PY_VER:~2,1%/site-packages/vmtk" ^
+    -DVMTK_CONTRIB_SCRIPTS:BOOL=ON ^
+	-DVMTK_PYTHON_VERSION:STRING="python%PY_VER%" ^
+	-DVMTK_USE_SUPERBUILD:BOOL=OFF
 if errorlevel 1 exit 1
 
 ninja install
--- vtk/bld.bat
+++ vtk/bld.bat
@@ -1,43 +1,31 @@
-@echo off
-setlocal EnableDelayedExpansion
-
 mkdir wk
 cd wk
 
-if "%PY_VER%" == "3.4" (
-    set GENERATOR=Visual Studio 10 2010
-) else (
-    if "%PY_VER%" == "3.5" (
-        set GENERATOR=Visual Studio 14 2015
-    ) else (
-      if "%PY_VER%" == "3.6" (
-          set GENERATOR=Visual Studio 15 2017
-      ) else (
-        set GENERATOR=Visual Studio 9 2008
-      )
-    )
-)
+set BUILD_CONFIG=Release
 
-if %ARCH% EQU 64 (
-    set GENERATOR=%GENERATOR% Win64
-)
-set BACKEND=OpenGL
+:: tell cmake where Python is
+set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
 
-cmake -LAH .. -G"%GENERATOR%" ^
--DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
--DCMAKE_BUILD_TYPE=Release ^
--DINSTALL_BIN_DIR=%LIBRARY_BIN% ^
--DINSTALL_INC_DIR=%LIBRARY_INC% ^
--DINSTALL_LIB_DIR=%LIBRARY_LIB% ^
--DBUILD_SHARED_LIBS=1 ^
--DBUILD_EXAMPLES=0 ^
--DBUILD_TESTING=0 ^
--DBUILD_DOCUMENTATION=0 ^
--DBUILD_SHARED_LIBS=1 ^
--DPYTHON_EXECUTABLE=%PYTHON% ^
--DVTK_WRAP_PYTHON=1 ^
--DVTK_RENDERING_BACKEND=%BACKEND% ^
--DVTK_INSTALL_PYTHON_MODULE_DIR=%SP_DIR%
+cmake .. -G "Ninja" ^
+    -Wno-dev ^
+    -DCMAKE_BUILD_TYPE=%BUILD_CONFIG% ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DBUILD_DOCUMENTATION:BOOL=OFF ^
+    -DBUILD_TESTING:BOOL=OFF ^
+    -DBUILD_EXAMPLES:BOOL=OFF ^
+    -DBUILD_SHARED_LIBS:BOOL=ON ^
+    -DPYTHON_EXECUTABLE:FILEPATH="%PYTHON%" ^
+    -DPYTHON_INCLUDE_DIR:PATH="%PREFIX%\include" ^
+    -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
+    -DVTK_USE_SYSTEM_ZLIB:BOOL=ON ^
+    -DModule_vtkPythonInterpreter:BOOL=OFF ^
+    -DVTK_WRAP_PYTHON:BOOL=ON ^
+    -DVTK_PYTHON_VERSION:STRING="%PY_VER%" ^
+    -DVTK_INSTALL_PYTHON_MODULE_DIR:PATH="%SP_DIR%" ^
+    -DVTK_HAS_FEENABLEEXCEPT:BOOL=OFF ^
+    -DVTK_RENDERING_BACKEND=OpenGL2 ^
+    -DModule_vtkRenderingMatplotlib=ON 
+if errorlevel 1 exit 1
 
-cmake --build . --config Release --target ALL_BUILD
-cmake --build . --config Release --target INSTALL
+ninja install
+if errorlevel 1 exit 1
